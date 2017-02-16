926cb7951f0a28429b37ba69d4c4460c5668ae24
Ramon Uchôa Gonçalves de Pinho <ramonuchoa386@gmail.com>
2017-02-16 21:29:34 -0200

    'init'

---
+++ assets/js/functions.js
@@ -0,0 +1,9 @@
+$(function () {
+
+  //auto expand textarea
+  function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+  }
+
+})
---
+++ contactengine.php
@@ -0,0 +1,36 @@
+<?php
+  $EmailTo = "contato@armazemvaledaspalmeiras.com.br";
+  $Name = Trim(stripslashes($_POST['nome']));
+  $Email = Trim(stripslashes($_POST['email']));
+  $Message = Trim(stripslashes($_POST['mensagem']));
+
+  // validation
+  $validationOK=true;
+    if (!$validationOK) {
+      print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+      exit;
+    }
+
+  // prepare email body text
+  $Body = "";
+  $Body .= "Nome: ";
+  $Body .= $Name;
+  $Body .= "\n";
+  $Body .= "Email: ";
+  $Body .= $Email;
+  $Body .= "\n";
+  $Body .= "Mensagem: ";
+  $Body .= $Message;
+  $Body .= "\n";
+
+  // send email
+  $success = mail($EmailTo, $Body, "From: <$Email>");
+
+  // redirect to success page
+    if ($success){
+      print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+    }
+    else{
+      print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+    }
+?>
